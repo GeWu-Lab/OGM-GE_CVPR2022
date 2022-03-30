8f51c8bd22ad159dc86611a2221db5a45a6a9058
AndongDeng <andongdeng69@gmail.com>
2022-03-30 16:03:57 +0800

    update

---
+++ utils/evaluation.py
@@ -0,0 +1,16 @@
+import torch
+
+
+def obtain_top1_accuracy(output, target):
+    with torch.no_grad():
+        batch_size = output.size(0)
+
+        _, pred = output.topk(1, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        correct_k = correct[:1].reshape(-1).float().sum(0, keepdims=True)
+        top1 = correct_k.mul_(100.0 / batch_size)
+
+    return top1
+
---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+def weight_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_normal_(m.weight)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
